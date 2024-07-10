3f8db970a37e354ad19f3569832917da72c79a21
AndreDias <dedasgdias@gmail.com>
2024-07-10 11:06:34 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '#scroll-content': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',     /* Firefox */
+        },
+        '#scroll-content::-webkit-scrollbar': {
+          'display': 'none',             /* Chrome, Safari, and Opera */
+        },
+      });
+    },
+  ],
+}
+
